9bbb444de33d64029ded91fe506f726645ea364b
Your Name <you@example.com>
2024-03-13 16:49:12 +0800

    can yong

--- client/client.go
+++ client/client.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -107,10 +106,7 @@ func (c *ClientEndpoint) Start() {
 			mu.Lock() // 在访问共享资源前锁定
 			conn, ok := conns[addr.String()]
 			if !ok {
-				udpconn, err := dialUDP("udp", &net.UDPAddr{
-					IP:   net.ParseIP("10.0.7.2"), // Replace with your source IP
-					Port: 6666,                    // Replace with your source port
-				}, addr.(*net.UDPAddr))
+				udpconn, err := net.DialUDP("udp", nil, addr.(*net.UDPAddr))
 				// Create a new UDP co //&net.UDPAddr{
 				//IP:   net.ParseIP("10.0.7.2"), // Replace with your source IP
 				//Port: 6666,                    // Replace with your source port
@@ -202,101 +198,4 @@ func handshake(ctx context.Context, stream *quic.Stream, hsh *tunnel.HandshakeHe
 		logger.Errorw("handshake error!", "error", "received an unknow ack info")
 		return false, nil
 	}
-}
-
-// DialUDP connects to the remote address raddr on the network net,
-// which must be "udp", "udp4", or "udp6".  If laddr is not nil, it is
-// used as the local address for the connection.
-func dialUDP(network string, laddr *net.UDPAddr, raddr *net.UDPAddr) (net.Conn, error) {
-	remoteSocketAddress, err := udpAddrToSocketAddr(raddr)
-	if err != nil {
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("build destination socket address: %s", err)}
-	}
-
-	localSocketAddress, err := udpAddrToSocketAddr(laddr)
-	if err != nil {
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("build local socket address: %s", err)}
-	}
-
-	fileDescriptor, err := unix.Socket(udpAddrFamily(network, laddr, raddr), unix.SOCK_DGRAM, 0)
-	if err != nil {
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("socket open: %s", err)}
-	}
-
-	if err = unix.SetsockoptInt(fileDescriptor, unix.SOL_IP, unix.IP_TRANSPARENT, 1); err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("set socket option: IP_TRANSPARENT: %s", err)}
-	}
-
-	if err = unix.SetsockoptInt(fileDescriptor, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("set socket option: SO_REUSEADDR: %s", err)}
-	}
-	if err = unix.SetsockoptInt(fileDescriptor, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("set socket option: SO_REUSEPORT: %s", err)}
-	}
-
-	if err = unix.Bind(fileDescriptor, localSocketAddress); err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("socket bind %v: %s", laddr, err)}
-	}
-
-	if err = unix.Connect(fileDescriptor, remoteSocketAddress); err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("socket connect: %s", err)}
-	}
-
-	fdFile := os.NewFile(uintptr(fileDescriptor), fmt.Sprintf("net-udp-dial-%s", raddr.String()))
-	defer fdFile.Close()
-
-	remoteConn, err := net.FileConn(fdFile)
-	if err != nil {
-		unix.Close(fileDescriptor)
-		return nil, &net.OpError{Op: "dial", Err: fmt.Errorf("convert file descriptor to connection: %s", err)}
-	}
-
-	return remoteConn, nil
-}
-
-// udpAddToSockerAddr will convert a UDPAddr
-// into a Sockaddr that may be used when
-// connecting and binding sockets
-func udpAddrToSocketAddr(addr *net.UDPAddr) (unix.Sockaddr, error) {
-	switch {
-	case addr.IP.To4() != nil:
-		ip := [4]byte{}
-		copy(ip[:], addr.IP.To4())
-
-		return &unix.SockaddrInet4{Addr: ip, Port: addr.Port}, nil
-
-	default:
-		ip := [16]byte{}
-		copy(ip[:], addr.IP.To16())
-
-		zoneID, err := strconv.ParseUint(addr.Zone, 10, 32)
-		if err != nil {
-			return nil, err
-		}
-
-		return &unix.SockaddrInet6{Addr: ip, Port: addr.Port, ZoneId: uint32(zoneID)}, nil
-	}
-}
-
-// udpAddrFamily will attempt to work
-// out the address family based on the
-// network and UDP addresses
-func udpAddrFamily(net string, laddr, raddr *net.UDPAddr) int {
-	switch net[len(net)-1] {
-	case '4':
-		return unix.AF_INET
-	case '6':
-		return unix.AF_INET6
-	}
-
-	if (laddr == nil || laddr.IP.To4() != nil) &&
-		(raddr == nil || laddr.IP.To4() != nil) {
-		return unix.AF_INET
-	}
-	return unix.AF_INET6
 }
