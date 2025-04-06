541e2114f32672fca0d1b8f656181601283b365a
Your Name <you@example.com>
2025-04-06 21:54:50 +0800

    change iptables

--- client/cmd/iptab.sh
+++ client/cmd/iptab.sh
@@ -16,6 +16,11 @@ ip route add local 0.0.0.0/0 dev lo table 100
 # ip route add default via 10.0.3.2 dev r1-eth3 table 3
 
 iptables -t mangle -F
+iptables -t mangle -X GOST
+iptables -t mangle -X GOST_LOCAL
+
+sudo iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o enx344b50000000 -j MASQUERADE
 
 iptables -t mangle -N GOST
 
@@ -30,28 +35,33 @@ iptables -t mangle -A GOST -p udp -d 127.0.0.0/8 -j RETURN
 iptables -t mangle -A GOST -p udp -d 255.255.255.255/32 -j RETURN
 iptables -t mangle -A GOST -p udp -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A GOST -p udp -d 10.0.0.0/24 -j RETURN
-iptables -t mangle -A GOST -p udp -d 10.0.1.1/32 -j RETURN
-iptables -t mangle -A GOST -p udp -d 10.0.2.1/32 -j RETURN
-iptables -t mangle -A GOST -p udp -d 10.0.3.1/32 -j RETURN
+iptables -t mangle -A GOST -p udp -d 169.254.0.0/16 -j RETURN
+# iptables -t mangle -A GOST -p udp -d 10.0.1.1/32 -j RETURN
+# iptables -t mangle -A GOST -p udp -d 10.0.2.1/32 -j RETURN
+# iptables -t mangle -A GOST -p udp -d 10.0.3.1/32 -j RETURN
+# iptables -t mangle -A GOST -p udp -d 10.0.4.2/32 -j RETURN
 iptables -t mangle -A GOST -p udp -m mark --mark 100 -j RETURN 
 iptables -t mangle -A GOST -p udp -m mark --mark 0x1 -j RETURN 
-iptables -t mangle -A GOST -p udp -j TPROXY --tproxy-mark 0x1/0x1 --on-ip 127.0.0.1 --on-port 12345 
+iptables -t mangle -A GOST -p udp -s 10.0.0.1 -j TPROXY --tproxy-mark 0x1/0x1 --on-ip 127.0.0.1 --on-port 12345
 iptables -t mangle -A PREROUTING -p udp -j GOST
 
 iptables -t mangle -N GOST_LOCAL
 
 # 排除DNS服务器的流量
-iptables -t mangle -A GOST -p udp -d 8.8.8.8 -j RETURN
-iptables -t mangle -A GOST -p udp -d 8.8.4.4 -j RETURN
-iptables -t mangle -A GOST -p udp -d 1.1.1.1 -j RETURN
-iptables -t mangle -A GOST -p udp -d 9.9.9.9 -j RETURN
-iptables -t mangle -A GOST -p udp -d 114.114.114.114 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 8.8.8.8 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 8.8.4.4 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 1.1.1.1 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 9.9.9.9 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 114.114.114.114 -j RETURN
 
 iptables -t mangle -A GOST_LOCAL -p udp -d 127.0.0.0/8 -j RETURN
 iptables -t mangle -A GOST_LOCAL -p udp -d 255.255.255.255/32 -j RETURN
 iptables -t mangle -A GOST_LOCAL -p udp -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A GOST_LOCAL -p udp -d 10.0.0.0/24 -j RETURN
-iptables -t mangle -A GOST_LOCAL -p udp -d 10.0.7.1/32 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 169.254.0.0/16 -j RETURN
+# iptables -t mangle -A GOST_LOCAL -p udp -d 10.0.7.1/32 -j RETURN
+# iptables -t mangle -A GOST_LOCAL -p udp -d 10.0.4.2/32 -j RETURN
+iptables -t mangle -A GOST_LOCAL -p udp -d 119.45.114.162/32 -j RETURN
 iptables -t mangle -A GOST_LOCAL -p udp -m mark --mark 100 -j RETURN 
 iptables -t mangle -A GOST_LOCAL -p udp -m mark --mark 0x1 -j RETURN 
 iptables -t mangle -A GOST_LOCAL -p udp -j MARK --set-mark 1
